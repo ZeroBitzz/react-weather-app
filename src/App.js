10635e4a97dda925abeabeecb57944d0afafd969
Broderick <zerothesu@gmail.com>
2024-10-08 14:09:50 -0600

    started getting api to work

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './App.css'
+// import { useState, useEffect } from 'react'
+
+// const locationData = 'https://api.openweathermap.org/geo/1.0/direct?q=london&limit=5&appid=4a95e5cd2ec6b313c75d4a7c3b046b39'
+// const weatherData = `https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid=1062393e2af2bbfdd204aedcdda64ac0`
 
 function App() {
+
+  // const [temp, setTemp] = useState(0)
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const result = await fetch(weatherData)
+  //     console.log(result)
+  //   }
+  //   fetchData()
+  // }, [])
+
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <h1>The weather </h1>
     </div>
   )
 }
