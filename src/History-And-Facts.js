d1c967e5eed217ff23d6f33632d64b139f87a08f
Broderick <zerothesu@gmail.com>
2024-10-09 12:36:02 -0600

    set up history and facts component

--- src/History-And-Facts.js
+++ src/History-And-Facts.js
@@ -2,7 +2,21 @@ import './History-And-Facts.css'
 
 function HistoryAndFacts(){
     return (
-        <div>History and facts</div>
+        <div className='history-and-facts'>
+            <div className='history'>
+                <h2>Search History</h2>
+                <span>History1</span>
+                <span>History2</span>
+                <span>History3</span>
+                <span>History4</span>
+                <span>History5</span>
+            </div>
+
+            <div className='facts'>
+                <h3>Fun Weather Fact</h3>
+                <span>Hot weather sucks!</span>
+            </div>
+        </div>
     )
 }
 
