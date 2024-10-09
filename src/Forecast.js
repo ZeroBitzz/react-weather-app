1ebc23d089ebecfe4e97cee5c49865981588288e
Broderick <zerothesu@gmail.com>
2024-10-09 12:25:08 -0600

    set up 5 day forecast component

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,8 +1,40 @@
 import './Forecast.css'
+import sunnyIcon from './Icons/sunny-icon.svg'
 
 function Forecast(){
     return (
-        <div>forecast</div>
+        <div className='five-day-forecast'>
+            <h3>5 Day Forecast</h3>
+            <div className='day-forecast'>
+                <span>weather</span>
+                <span>temp</span>
+                <img src={sunnyIcon} alt='sunny icon'/>
+            </div>
+
+            <div className='day-forecast'>
+                <span>weather</span>
+                <span>temp</span>
+                <img src={sunnyIcon} alt='sunny icon'/>
+            </div>
+
+            <div className='day-forecast'>
+                <span>weather</span>
+                <span>temp</span>
+                <img src={sunnyIcon} alt='sunny icon'/>
+            </div>
+
+            <div className='day-forecast'>
+                <span>weather</span>
+                <span>temp</span>
+                <img src={sunnyIcon} alt='sunny icon'/>
+            </div>
+
+            <div className='day-forecast'>
+                <span>weather</span>
+                <span>temp</span>
+                <img src={sunnyIcon} alt='sunny icon'/>
+            </div>
+        </div>
     )
 }
 
