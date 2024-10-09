bf974f42a7a1123e3a2cd8234fb77b387dbb10cf
Broderick <zerothesu@gmail.com>
2024-10-09 12:43:57 -0600

    minor css changes

--- src/Forecast.js
+++ src/Forecast.js
@@ -5,6 +5,7 @@ function Forecast(){
     return (
         <div className='five-day-forecast'>
             <h3>5 Day Forecast</h3>
+            <hr />
             <div className='day-forecast'>
                 <span>weather</span>
                 <span>temp</span>
