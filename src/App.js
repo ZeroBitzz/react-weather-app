9c8f099353d24c67b70aaece91307028d992b0cf
Broderick <zerothesu@gmail.com>
2024-10-09 13:06:39 -0600

    css changes, added thermostat

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css'
 import Forecast from './Forecast'
 import HistoryAndFacts from './History-And-Facts'
+import weatherman from './weatherman/weatherman.svg'
+import sunnyIcon from './Icons/sunny-icon.svg'
 // import { useState, useEffect } from 'react'
 
 // const locationData = 'https://api.openweathermap.org/geo/1.0/direct?q=london&limit=5&appid=4a95e5cd2ec6b313c75d4a7c3b046b39'
@@ -21,7 +23,24 @@ function App() {
   return (
     <div className="App">
       <Forecast />
-      <h1>Brodericks Weather App</h1>
+      <div>
+
+        <div className='main-content'>
+          <div>
+            <h1>Brodericks Weather App</h1>
+            <form>
+              <input className='location-input' type='text' placeholder='Type Location Here'/>
+            </form>
+            <h2>Weather Condition / Temp</h2>
+            <img className='weather-icon' src={sunnyIcon}/>
+          </div>
+
+          <div>
+            <img className='weatherman' src={weatherman}/>
+          </div>
+        </div>
+
+      </div>
       <HistoryAndFacts />
     </div>
   )
--- src/History-And-Facts.js
+++ src/History-And-Facts.js
@@ -3,14 +3,25 @@ import './History-And-Facts.css'
 function HistoryAndFacts(){
     return (
         <div className='history-and-facts'>
+            <div>
+                <h2>Thermostat</h2>
+                <form className='thermostat-form'>
+                    <input className='thermostat' type='text' placeholder='Custom Temp(F)?'/>
+                </form>
+            </div>
+
+            <hr />
+
             <div className='history'>
-                <h2>Search History</h2>
+                <h2 className='search-history-h2'>Search History</h2>
                 <span>History1</span>
                 <span>History2</span>
                 <span>History3</span>
                 <span>History4</span>
                 <span>History5</span>
             </div>
+
+            <hr />
 
             <div className='facts'>
                 <h3>Fun Weather Fact</h3>
