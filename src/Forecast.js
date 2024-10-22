776fa1f984408ff7d0aa194cb912ebe1e851fa39
Broderick <zerothesu@gmail.com>
2024-10-22 16:11:31 -0600

    added date element to 5 day forecast

--- src/Forecast.js
+++ src/Forecast.js
@@ -6,33 +6,58 @@ function Forecast(){
             <h3>5 Day Forecast</h3>
             <hr />
                 <div className='day-forecast hide' id='forecast1'>
-                    <span id='1-day-forecast-weather-condition'>weather</span>
-                    <span id='1-day-forecast-temperature'>temp</span>
-                    <img src={null} id='forecast1-icon' alt='sunny icon'/>
+                    <div>
+                        <span id='forecast1-date'>Date:</span>
+                    </div>
+                    <div className='weather-temp-icon-group'>
+                        <span id='1-day-forecast-weather-condition'>weather</span>
+                        <span id='1-day-forecast-temperature'>temp</span>
+                        <img src={null} id='forecast1-icon' alt='sunny icon'/>
+                    </div>
                 </div>
 
                 <div className='day-forecast hide' id='forecast2'>
-                    <span id='2-day-forecast-weather-condition'>weather</span>
-                    <span id='2-day-forecast-temperature'>temp</span>
-                    <img src={null} id='forecast2-icon' alt='sunny icon'/>
+                    <div>
+                        <span id='forecast2-date'>Date:</span>
+                    </div>
+                    <div className='weather-temp-icon-group'>
+                        <span id='2-day-forecast-weather-condition'>weather</span>
+                        <span id='2-day-forecast-temperature'>temp</span>
+                        <img src={null} id='forecast2-icon' alt='sunny icon'/>
+                    </div>
                 </div>
 
                 <div className='day-forecast hide' id='forecast3'>
-                    <span id='3-day-forecast-weather-condition'>weather</span>
-                    <span id='3-day-forecast-temperature'>temp</span>
-                    <img src={null} id='forecast3-icon' alt='sunny icon'/>
+                    <div>
+                        <span id='forecast3-date'>Date:</span>
+                    </div>
+                    <div className='weather-temp-icon-group'>
+                        <span id='3-day-forecast-weather-condition'>weather</span>
+                        <span id='3-day-forecast-temperature'>temp</span>
+                        <img src={null} id='forecast3-icon' alt='sunny icon'/>
+                    </div>
                 </div>
 
                 <div className='day-forecast hide' id='forecast4'>
-                    <span id='4-day-forecast-weather-condition'>weather</span>
-                    <span id='4-day-forecast-temperature'>temp</span>
-                    <img src={null} id='forecast4-icon' alt='sunny icon'/>
+                    <div>
+                        <span id='forecast4-date'>Date:</span>
+                    </div>
+                    <div className='weather-temp-icon-group'>
+                        <span id='4-day-forecast-weather-condition'>weather</span>
+                        <span id='4-day-forecast-temperature'>temp</span>
+                        <img src={null} id='forecast4-icon' alt='sunny icon'/>
+                    </div>
                 </div>
 
                 <div className='day-forecast hide' id='forecast5'>
-                    <span id='5-day-forecast-weather-condition'>weather</span>
-                    <span id='5-day-forecast-temperature'>temp</span>
-                    <img src={null} id='forecast5-icon' alt='sunny icon'/>
+                    <div>
+                        <span id='forecast5-date'>Date:</span>
+                    </div>
+                    <div className='weather-temp-icon-group'>
+                        <span id='5-day-forecast-weather-condition'>weather</span>
+                        <span id='5-day-forecast-temperature'>temp</span>
+                        <img src={null} id='forecast5-icon' alt='sunny icon'/>
+                    </div>
                 </div>
         </div>
     )
