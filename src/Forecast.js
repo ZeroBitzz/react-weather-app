5d6d9e3c23c03f5c4c97de79fc652b32b26e27ef
Broderick <zerothesu@gmail.com>
2024-10-22 14:35:39 -0600

    reverted back so there is no hidden css elements

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,4 +1,5 @@
 import './Forecast.css'
+import sunnyIcon from './Icons/sunny-icon.svg'
 
 function Forecast(){
     return (
@@ -6,33 +7,33 @@ function Forecast(){
             <h3>5 Day Forecast</h3>
             <hr />
             <div className='day-forecast'>
-                <span id='1-day-forecast-weather-condition' className='hide'>weather</span>
-                <span id='1-day-forecast-temperature' className='hide'>temp</span>
-                <img src={null} alt='sunny icon' className='hide'/>
+                <span id='1-day-forecast-weather-condition'>weather</span>
+                <span id='1-day-forecast-temperature'>temp</span>
+                <img src={sunnyIcon} alt='sunny icon'/>
             </div>
 
             <div className='day-forecast'>
-                <span id='2-day-forecast-weather-condition' className='hide'>weather</span>
-                <span id='2-day-forecast-temperature' className='hide'>temp</span>
-                <img src={null} alt='sunny icon' className='hide'/>
+                <span id='2-day-forecast-weather-condition'>weather</span>
+                <span id='2-day-forecast-temperature'>temp</span>
+                <img src={sunnyIcon} alt='sunny icon'/>
             </div>
 
             <div className='day-forecast'>
-                <span id='3-day-forecast-weather-condition' className='hide'>weather</span>
-                <span id='3-day-forecast-temperature' className='hide'>temp</span>
-                <img src={null} alt='sunny icon' className='hide'/>
+                <span id='3-day-forecast-weather-condition'>weather</span>
+                <span id='3-day-forecast-temperature'>temp</span>
+                <img src={sunnyIcon} alt='sunny icon'/>
             </div>
 
             <div className='day-forecast'>
-                <span id='4-day-forecast-weather-condition' className='hide'>weather</span>
-                <span id='4-day-forecast-temperature' className='hide'>temp</span>
-                <img src={null} alt='sunny icon' className='hide'/>
+                <span id='4-day-forecast-weather-condition'>weather</span>
+                <span id='4-day-forecast-temperature'>temp</span>
+                <img src={sunnyIcon} alt='sunny icon'/>
             </div>
 
             <div className='day-forecast'>
-                <span id='5-day-forecast-weather-condition' className='hide'>weather</span>
-                <span id='5-day-forecast-temperature' className='hide'>temp</span>
-                <img src={null} alt='sunny icon' className='hide'/>
+                <span id='5-day-forecast-weather-condition'>weather</span>
+                <span id='5-day-forecast-temperature'>temp</span>
+                <img src={sunnyIcon} alt='sunny icon'/>
             </div>
         </div>
     )
